d9557cd5b4674f29b0b50adc4dbaefe76c91164a
KangHannarae <hann52156@gmail.com>
2025-06-05 15:16:01 +0900

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || 'mysql-ha3n.railway.internal',
+  host: process.env.MYSQLHOST || 'shortline.proxy.rlwy.net',
   user: process.env.MYSQLUSER || 'root',
   password: process.env.MYSQLPASSWORD || 'HpTXSrpmoVXRkmybWkqNxBvIKoFiHPBW',
-  database: process.env.MYSQLDATABASE || 'railway',
-  port: process.env.MYSQLPORT || 3306,
+  database: process.env.MYSQLDATABASE || 'money',
+  port: Number(process.env.MYSQLPORT) || 11530,
 });
 
 const db = pool.promise();
