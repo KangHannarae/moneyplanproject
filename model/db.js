ed9d00b5fd21cead3411fb8d56f45f5b7dae5f13
KangHannarae <hann52156@gmail.com>
2025-06-05 15:23:48 +0900

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,5 +1,11 @@
 import mysql from 'mysql2';
 
+console.log('host:', process.env.MYSQLHOST);
+console.log('user:', process.env.MYSQLUSER);
+console.log('password:', process.env.MYSQLPASSWORD ? '****' : '없음');
+console.log('database:', process.env.MYSQLDATABASE);
+console.log('port:', process.env.MYSQLPORT);
+
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST || 'shortline.proxy.rlwy.net',
   user: process.env.MYSQLUSER || 'root',
