b8430c31bb1025009c95e72a5cee468a91568e4f
KangHannarae <hann52156@gmail.com>
2025-06-05 16:33:29 +0900

    Update PostController.js

--- controller/PostController.js
+++ controller/PostController.js
@@ -1,6 +1,10 @@
 import multer from "multer";
 import fs from "fs/promises";
 import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import {
   getPostListFromDB, insertPostToDB, getFilenameById, incrementDownloadCount,
   incrementHitCount, getPostById, getCommentsByPostId,
